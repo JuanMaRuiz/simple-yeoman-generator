b379bfec84ee83481d14c2bcca3c1efb88a7cfe9
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-02-14 19:58:34 +0100

    Added demo test and needed dependencies

---
+++ test/demo-test.js
@@ -0,0 +1,9 @@
+var mocha = require('mocha'),
+    chai = require('chai'),
+    expect = chai.expect;
+
+describe('Demo test', function() {
+  it('should generate the folder', function() {
+    console.log("There is no test implemented yet!");
+  });
+});
